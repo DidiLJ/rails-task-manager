3348b1b72f53432370ea4aba4eb03a71c82b1555
Adrien LOUIS-JOSEPH <louisjosephadrien@gmail.com>
2025-05-20 15:47:10 +0200

    clear

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,17 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
+
+  # get '/task/:id', to: 'tasks#show', as: :task
+
+  # get '/new', to: 'tasks#new', as: :new
+  # post '/tasks', to: 'tasks#create'
+
+  # get '/edit/:id', to: 'tasks#edit', as: :edit
+  # patch '/task/:id', to: 'tasks#update'
+
+  # delete '/task/:id', to: 'tasks#delete'
+
+  resources :tasks
 end
